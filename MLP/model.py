0ffe9e9f017590b12a7ee7c62e5f1b96bab82c7c
GDB <13458466578@163.com>
2025-10-30 13:21:02 +0800

    The get_data_dog_and_cat(batch_size, img_size) function was added in the utils file to obtain the cat and dog image classification dataset, and the MLP model was modified at the same time - a fully connected layer was added.

--- MLP/model.py
+++ MLP/model.py
@@ -3,21 +3,25 @@ import torch.nn as nn
 
 
 class MLP(nn.Module):
-	def __init__(self, input_dim, hid_dim1, hid_dim2, output_dim, dropout):
+	def __init__(self, input_dim, hid_dim1, hid_dim2, hid_dim3, output_dim, dropout):
 		super(MLP, self).__init__()
 
 		# nn.Linear 是一个全连接层：y = Wx + b
 		self.fc1 = nn.Linear(input_dim, hid_dim1)
 		self.fc2 = nn.Linear(hid_dim1, hid_dim2)
-		self.output = nn.Linear(hid_dim2, output_dim)
+		self.fc3 = nn.Linear(hid_dim2, hid_dim3)
+		self.output = nn.Linear(hid_dim3, output_dim)
 		self.dropout = nn.Dropout(dropout)
 
 	def forward(self, x):
 		# 第一层：输入 x 经过 fc1 + ReLU 激活 + Dropout
 		x = torch.relu(self.fc1(x))
 		x = self.dropout(x)
-		# 第二次：输入 x 经过 fc2 + ReLU 激活 + Dropout
+		# 第二层：输入 x 经过 fc2 + ReLU 激活 + Dropout
 		x = torch.relu(self.fc2(x))
+		x = self.dropout(x)
+		# 第三层：输入 x 经过 fc3 + ReLU 激活 + Dropout
+		x = torch.relu(self.fc3(x))
 		x = self.dropout(x)
 		# 输出层：经过 output 层
 		x = self.output(x)
--- utils/utils.py
+++ utils/utils.py
@@ -1,13 +1,13 @@
 from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
-from torchvision.datasets import MNIST
+from torchvision.datasets import MNIST, ImageFolder
 
 
 def get_data_MNIST(batch_size):
 	"""
 	获取 MNIST 数据集，分为训练集和测试集使用
 	:param batch_size: 批处理的大小
-	:return: 训练集和测试集加载器（DataLoader）
+	:return: 训练集和验证集加载器（DataLoader）
 	"""
 	# transforms.Compose()是一个容器，可以将多个转换操作组合在一起，返回一个组合后的转换操作。然后传递给 DataLoader。这样，当加载数据时，这些转换操作会自动应用到每一张图像上。
 	transform = transforms.Compose([
@@ -23,6 +23,33 @@ def get_data_MNIST(batch_size):
 	# 创建数据加载器，用于加载数据集并将其分批次地传递给模型
 	train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 	val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+	return train_loader, val_loader
+
+
+def get_data_dog_and_cat(batch_size, img_size):
+	"""
+	获取 dog_and_cat 数据集，分为训练集和测试集使用
+	:param batch_size: 批处理的大小
+	:param img_size: 图片统一的尺寸大小
+	:return: 训练集和验证集加载器（DataLoader）
+	"""
+	train_dir = '../data/dog_and_cat/training_set'
+	test_dir = '../data/dog_and_cat/test_set'
+
+	transform = transforms.Compose([
+		# 统一图片尺寸
+		transforms.Resize((img_size, img_size)),
+		# 转为 [0,1]
+		transforms.ToTensor(),
+		# 将每个像素值标准化，将其范围缩放到[-1.0, 1.0],有助于加速训练和提高网络的收敛速度
+		transforms.Normalize((0.5,), (0.5,))
+	])
+
+	train_dataset = ImageFolder(root=train_dir, transform=transform)
+	test_dataset = ImageFolder(root=test_dir, transform=transform)
+	train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+	val_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 	return train_loader, val_loader
 
